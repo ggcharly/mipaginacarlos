4829741b18cd7028ca8dec1d9f8198d57f1775ea
ggcharly <126518677+ggcharly@users.noreply.github.com>
2023-06-12 21:51:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".navegacion").
+                      classList.toggle("navegacion-responsive")})
+
+ function showAlert(){ alert('No estas haciendo lo correcto');}                     
